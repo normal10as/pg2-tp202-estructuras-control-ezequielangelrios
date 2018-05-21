1e38a2f824dbe635298e11830953e746f49df30a
USER <USER@LAPTOP-M91A736D>
2018-05-21 18:08:29 -0300

    hasta ejercicio2_7

---
+++ estructuras_control_flujo/estructuras_control_flujo/ejercicio2_7.vb
@@ -0,0 +1,26 @@
+﻿Module ejercicio2_7
+    Sub Main()
+
+        Dim angulo As Integer
+        Console.WriteLine("Ingrese el tamaño del angulo: ")
+        angulo = Console.ReadLine()
+        Select Case angulo
+            Case 0 To 90
+                Console.WriteLine("El angulo ingresado es AGUDO")
+            Case 90
+                Console.WriteLine("El angulo ingresado es RECTO")
+            Case 90 To 179
+                Console.WriteLine("El angulo ingresado es OBTUSO")
+            Case 180
+                Console.WriteLine("El angulo ingresado es LLANO")
+            Case 180 To 359
+                Console.WriteLine("El angulo ingresado es CÓNCAVO")
+            Case < 0
+                Console.WriteLine("Error")
+            Case > 360
+                Console.WriteLine("Error")
+        End Select
+        Console.ReadLine()
+    End Sub
+
+End Module
