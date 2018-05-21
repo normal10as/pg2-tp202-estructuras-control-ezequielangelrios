ce1b916ccbcf3de73f942df0387b90ce52c6f33f
USER <USER@LAPTOP-M91A736D>
2018-05-21 20:08:53 -0300

    realizando ejercicio 2_8

---
+++ estructuras_control_flujo/estructuras_control_flujo/ejercicio2_8.vb
@@ -0,0 +1,22 @@
+﻿Module ejercicio2_8
+    Sub Main()
+        Dim mes As Integer
+        Dim num As Integer
+        Console.WriteLine("ingrese el numero de mes:")
+        mes = Console.ReadLine
+        Console.WriteLine("ingrese el año: ")
+        num = Console.ReadLine
+        If (num Mod 4 = 0 And num Mod 100 <> 0 Or num Mod 400 = 0) Then
+            Console.WriteLine("el anio es bisiesto")
+        Else
+            Console.WriteLine("el anio no es bisiesto")
+
+        End If
+        Select Case mes
+            Case 1
+                Console.WriteLine("Enero")
+        End Select
+        Console.ReadLine()
+    End Sub
+
+End Module
